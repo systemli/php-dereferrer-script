13855f911fa19d0a19b030eff55776dabb45a1af
louis <louis@systemli.org>
2017-11-17 12:39:59 +0100

    add systemli logo

--- splash.php
+++ splash.php
@@ -4,24 +4,29 @@
     <meta charset="utf-8">
     <title>Dereferrer Service</title>
     <style>
+        html {
+            line-height: 1.15; /* 1 */
+            -ms-text-size-adjust: 100%; /* 2 */
+            -webkit-text-size-adjust: 100%; /* 2 */
+        }
+
         body {
             background-color: #fff;
-            padding: 0;
+            padding: 20px 0 0 0;
             margin: 0 auto;
             width: 900px;
             text-align: center;
+        }
+
+        h1 {
+            font-size: 2em;
+            margin: 0.67em 0;
         }
 
         h1, p {
             text-transform: uppercase;
             font-family: Verdana, Arial, Helvetica, sans-serif;
             color: #262B2E;
-            padding: 0;
-            margin: 0;
-        }
-
-        h1 {
-            font-size: 120px;
         }
 
         p {
@@ -43,8 +48,9 @@
     </style>
 </head>
 <body>
+<p><img src="logo.png" /></p>
 <h1>Dereferrer Service</h1>
 <p>This is a private dereferrer Service. Go run your own!</p>
-<div class="about">Get the <a href="http://www.naden.de/blog/derefer-script">Derefer Script</a></div>
+<div class="about">Hosted by <a href="https://www.systemli.org">systemli.org</a>. See the <a href="https://github.com/systemli/php-dereferrer-script">Source Code</a></div>
 </body>
 </html>
