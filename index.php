d6e443d78ba801662bb98d2734a62defeab64443
louis <louis@systemli.org>
2017-11-17 12:28:05 +0100

    typo

--- index.php
+++ index.php
@@ -10,20 +10,17 @@
  */
 
 // check if we got an url
-if(($pos = strpos($_SERVER['REQUEST_URI'], '?')) !== false) {
+if (($pos = strpos($_SERVER['REQUEST_URI'], '?')) !== false) {
 
-	// try to extract the url
+    // try to extract the url
     $url = urldecode(trim(substr($_SERVER['REQUEST_URI'], $pos + 1)));
-  
-	// if url is set, include the redirect page
-  if(!empty($url)) {  
-    include_once 'redirect.php';
-    exit;
-  }
+
+    // if url is set, include the redirect page
+    if (!empty($url)) {
+        include_once 'redirect.php';
+        exit;
+    }
 }
 
 // else show the splash page
-
 include_once 'splash.php';
-
-?>
